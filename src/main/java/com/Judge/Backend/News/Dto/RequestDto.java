7d67e3e156529b92bbb480ee2c7189abd247df85
Vaibhav Sidana <sidanavaibhav@gmail.com>
2025-08-21 21:41:37 +0530

    initial bootstrap

---
+++ src/main/java/com/Judge/Backend/News/Dto/RequestDto.java
@@ -0,0 +1,25 @@
+package com.Judge.Backend.News.Dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RequestDto {
+    @JsonProperty("info_type")
+    String infoType;
+
+    @JsonProperty("headline")
+    String headline;
+
+    @JsonProperty("information")
+    String information;
+
+    @JsonProperty("meta_data")
+    String metaData;
+}
---
+++ src/main/java/com/Judge/Backend/News/NewsController.java
@@ -0,0 +1,15 @@
+package com.Judge.Backend.News;
+
+import com.Judge.Backend.News.Dto.RequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewsController {
+    @PostMapping("/verify/news")
+    public ResponseEntity<?> verifyNews(RequestDto requestDto) {
+
+        return ResponseEntity.ok().build();
+    }
+}
