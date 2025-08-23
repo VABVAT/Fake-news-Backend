40536cba6d485b8849ae55f80403bb3809b5c0f9
Vaibhav Sidana <sidanavaibhav@gmail.com>
2025-08-23 14:19:38 +0530

    api changes before adding ground search

--- src/main/java/com/Judge/Backend/News/Service/GeminiParsingService.java
+++ src/main/java/com/Judge/Backend/News/Service/GeminiParsingService.java
@@ -2,22 +2,38 @@ package com.Judge.Backend.News.Service;
 
 import com.Judge.Backend.News.Dto.FakeNewsVerdictDto;
 import com.Judge.Backend.News.Dto.RequestDto;
+import com.Judge.Backend.News.Util.GeminiConfig;
 import com.Judge.Backend.News.Util.GeminiPrompt;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @AllArgsConstructor
-@NoArgsConstructor()
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class GeminiParsingService {
     GeminiPrompt geminiPrompt;
+    GeminiConfig geminiConfig;
 
-    FakeNewsVerdictDto fakeNews(RequestDto dto) {
-        String GeminiPrompt = geminiPrompt.buildPrompt(dto);
-        return null;
+
+    public FakeNewsVerdictDto fakeNews(RequestDto dto) {
+        try {
+            String prompt = geminiPrompt.buildPrompt(dto);
+            String apiKey = geminiConfig.getGeminiApiKey();
+
+
+        } catch (Exception e) {
+            // Proper error handling
+
+        }
     }
-
 }
---
+++ src/main/java/com/Judge/Backend/News/Util/GeminiConfig.java
@@ -0,0 +1,21 @@
+package com.Judge.Backend.News.Util;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class GeminiConfig {
+
+    @Value("${gemini.api.key}")
+    String geminiApiKey;
+
+}
